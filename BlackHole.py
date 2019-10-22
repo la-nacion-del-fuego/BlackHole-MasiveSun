6e2fabcf3bfa1a10d13b7379fa75193edf2f0a6a
Donovan Mosheh Ramirez Trejo <38016639+soyeldono@users.noreply.github.com>
2019-10-22 18:35:18 -0500

    Nuevo Update
    Aun falta añadir que el agujero negro gane masa del sol

--- BlackHole.py
+++ BlackHole.py
@@ -89,15 +89,26 @@ class Particle:
         self.v[2] += v[2]
 
     def updatePosition(self,time,save):        
-        self.p = [self.p[0]+ (self.v[0]) *dt,self.p[1]+ (self.v[1])*dt,self.p[2]+ (self.v[2])*dt]
+        self.p = [self.p[0]+(self.v[0])*dt, self.p[1]+(self.v[1])*dt, self.p[2]+(self.v[2])*dt]
         if save:
             self.time.append(time)
             self.trajectory.append(self.p)
-
+        
+        #self.reduceMass(4.594781899714635140644109253974)
 
     def getTrajectory(self):
         return self.time, self.trajectory
-       
+
+    def updateMass(self,Particle1:"bhole",Particle2:"Canis"):
+        distance = math.sqrt( math.pow(Particle2.p[0]-Particle1.p[0],2) + math.pow(Particle2.p[1] - Particle1.p[1],2) + math.pow(Particle2.p[2]-Particle1.p[2],2))
+        
+        if distance <= 0.15e10:
+            Particle1.m = Particle1.m + (Particle2.m - (Particle2 - 8e10))
+            Particle2.m = (Particle2.m - (Particle2 - 8e10))
+
+        return Particle1.m,Particle2.m
+
+
 class Potential:
    
     def __init__(self, system, dt):
@@ -108,24 +119,28 @@ class Potential:
         for particle in self.system:
             for other in self.system:
                 if other != particle:
+                    m1,m2 = other.updateMass(particle,other)
+                    self.system[0].m = m1
+                    self.system[1].m = m2
                     velocity = particle.computeV(other)
                     particle.updateV(velocity)
         for particle in self.system:
             particle.updatePosition(time,save)
+            
 
         return self.system
 
-lenTime = 450000 # 1 -> 1000 años por lo que 100*1000 = 1 millon
-dt=10.0              #años
+#lenTime = 450000 # 1 -> 1000 años por lo que 100*1000 = 1 millon
+lenTime = 900000
+dt=10.0  #años
 
-bhole=Particle([0.0, 0.0, 0.0] ,[0.0, 0.0, 0.0],7.359e30) # posicion[km],v[km/años],m[MasaSolar]: El agujero negro tiene una masa solar de 3.7masasSolares == 7.259x10^30kg
-Canis = Particle([20e8, 0.0, 0.0] ,[0.0, 5e5, 0.0],33.813e30) # posicion[km],v[km/años],m[MasaSolar]: La estrella Canis tiene una masa solar de 17masasSolares == 33.813x10^30kg
+bhole=Particle([0.0, 0.0, 0.0] ,[0.0, 0.0, 0.0],7.359e30) # posicion[km],v[km/años],m[MasaSolar]: El agujero negro tiene una masa solar de 3.7masasSolares == (7.259x10^30)kg
+Canis = Particle([20e8, 0.0, 0.0] ,[0.0, 5e5, 0.0],33.813e30) # posicion[km],v[km/años],m[MasaSolar]: La estrella Canis tiene una masa solar de 17masasSolares == (33.813x10^30)kg
 
 n_steps = int(lenTime/dt)
 
 
 particles = [bhole, Canis]
-#particles = [sun,mercury,venus,earth]
 twoBody = Potential(particles,dt)
 
 x=[]
@@ -144,38 +159,6 @@ for t in range(1,n_steps):
    
 
 
-#B.setdt(dt)
-#x=[]
-#y=[]
-#v=[]
-#a=[]
-#x.append(0.0)
-##y.append(B.getPosition()[0])
-#y.append(B.getPosition())
-#v.append(B.getVelocity()[0])
-#
-#print(B.getVelocity()[0])
-#
-#a.append(0.0)
-#v1=B.getVelocity()[0]
-##lastX = B.getPosition()[0]
-#
-##for t in range(1,100):
-##    lastX = B.getPosition()[0]
-##    lastV = v1
-##    B.integrate(A)
-##    print(B.getPosition())
-##    x.append(float(t)*dt)
-##    y.append(B.getPosition()[0])
-##    v1=(B.getPosition()[0]-lastX)/B.dt
-##    print(v1)
-##    v.append(v1)
-##    a.append((v1-lastV)/B.dt)
-#
-#for t in range(1,100):
-#    B.integrate(A)
-#    x.append(float(t)*dt)
-#    y.append(B.getPosition())
 
 fig = plt.figure()
 
@@ -189,38 +172,6 @@ for particle in particles:
         ax.scatter(y[0], y[1], y[2], marker='o',c=c[i])
         #ax.scatter(y[0], y[1], y[2], c=c[i])
     i=i+1
-
-
-# stack the plots
-#lns = []
-#for i in range(len(t)):
-#    ln1, = ax.plot(y0[:i], y1[:i], z1[:i], 'o-', color='steelblue')
-#    ln2, = ax.plot(x2[:i], y2[:i], z2[:i], 'o-', color='darkorange')
-#    lns.append([ln1, ln2])
-
-#line_ani = animation.ArtistAnimation(fig, lns, interval=100, blit=True)
-
-   
-#for point in y:
-#    ax.scatter(point[0], point[1], point[2], marker='o')
-
-#pointA=A.getPosition()
-#ax.scatter(pointA[0], pointA[1], pointA[2], marker='o')
-
-   
-#fig, ax = plt.subplots(3)    
-#ax[0].plot(x,y)
-#ax[0].set(xlabel='time [sec]', ylabel='distance [km]',
-#           title="n-body")
-#ax[0].grid()
-#
-#ax[1].plot(x,v)
-#ax[1].set(xlabel='time [sec]', ylabel='velocity [km/s]')
-#ax[1].grid()
-#
-#ax[2].plot(x,a)
-#ax[2].set(xlabel='time [sec]', ylabel='acceleration [km/s^2]')
-#ax[2].grid()
 
 
 
